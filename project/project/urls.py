ce5eec9688293229f57cd0c79e776dcb42ac7ae0
Xenx3128 <Xenx3024@yandex.ru>
2022-12-14 20:00:30 +0500

    feat: add bootstrap make some templates make homepage template and view

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('homepage.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
