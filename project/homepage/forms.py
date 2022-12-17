d0f42826b261592c695e5412206d0021e4896a7c
miheisv <114287999+miheisv@users.noreply.github.com>
2022-12-17 22:58:02 +0300

    Created pagination

--- project/homepage/forms.py
+++ project/homepage/forms.py
@@ -12,6 +12,7 @@ class PostForm(forms.ModelForm):
             Post.image.field.name,
             Post.text.field.name,
             Post.category.field.name,
+            Post.tags.field.name,
         )
 
     def __init__(self, *args, **kwargs):
